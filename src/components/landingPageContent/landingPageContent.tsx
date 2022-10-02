ac4253f370a40887cec059005aaa1feb44407525
ajinkya.gholape <ajinkya.gholape@infraplan.co.in>
2022-10-02 21:10:05 +0530

    Landing Page - Main content UI Implementation

---
+++ src/components/landingPageContent/landingPageContent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./landingPageContent.css"
+function LandingPageContent() {
+    return (
+        <div className='landing-page-content-root'>
+        </div>
+    )
+}
+
+export default LandingPageContent
