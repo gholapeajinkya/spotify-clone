d121b1824dc5235b206908670fa67f81f83b4ec3
ajinkya.gholape <ajinkya.gholape@infraplan.co.in>
2022-10-03 08:47:55 +0530

    Landing page main content card

---
+++ src/components/wideCard/wideCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./wideCard.css"
+
+function WideCard() {
+    return (
+        <div className='wide-card-root'>
+            WideCard
+        </div>
+    )
+}
+
+export default WideCard
